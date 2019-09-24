a11fe7d84966ce997a2d8aab64ec3c7381fcfbd0
devnow8 <72devdev@gmail.com>
2019-09-25 06:11:32 +0900

    todos

---
+++ todo.js
@@ -0,0 +1,25 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput  = form.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue  = toDo
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !== null){
+    
+    }
+}
+
+function init () {
+        loadToDos();
+        toDoForm.addEventListener("submit", handleSubmit);
+
+}
+
+    init();
