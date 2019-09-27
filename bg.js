5c412fbc63d9f6fc287c330745621317e8407ca9
devnow8 <72devdev@gmail.com>
2019-09-28 02:04:54 +0900

    bg_img

---
+++ bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_N = 3;
+
+function paintImg(IMG_N){
+    const image = new Image();
+    image.src = `images/${IMG_N}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    
+}
+
+function genRandom(){
+    const number = Math.ceil(Math.random()*IMG_N);
+    return number;
+}
+function init(){
+    const randomNum = genRandom();
+    paintImg(randomNum);
+
+}
+
+init();
